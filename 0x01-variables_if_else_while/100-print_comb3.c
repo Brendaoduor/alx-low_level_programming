7c5bd79c98172bde516d9082f8d1895832efcb66
brendaoduor <bresharchie2@gmail.com>
2022-07-01 16:58:46 -0700

    work

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,12 +11,15 @@ int num;
 int num2;
 for (num = 0; num < 9; num++)
 {
-	for (num2 = 1; num2 < 10; num2++)
-
-	putchar(num);
-	putchar(num2);
+for (num2 = 1; num2 < 10; num2++)
+{
+	putchar(num % 10) + '0');
+	putchar(num2 % 10) + '0');
+if (num == 8 && num2 == 9)
+	continue;
 	putchar(',');
 	putchar(' ');
+}
 }
 	putchar('\n');
 return (0);
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,14 @@
 int main(void)
 {
 int num;
-for (num = 0; num < 10; num++)
+for (num = 0; num <= 10; num++)
 {
-	putchar(num);
+	putchar(num % 10) + '0');
+	if (num == 9)
+		continue;
 	putchar(',');
 	putchar(' ');
 }
-	putchar('\n')
+	putchar('\n');
 return (0);
 }
