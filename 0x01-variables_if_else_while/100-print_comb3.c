8c690bbb374056f9c017bfe054fee3f6a9985202
brendaoduor <bresharchie2@gmail.com>
2022-07-02 13:52:26 -0700

    work

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,8 +13,8 @@ for (num = 0; num < 9; num++)
 {
 for (num2 = num + 1; num2 < 10; num2++)
 {
-	putchar(num);
-	putchar(num2);
+	putchar((num % 10) + 0);
+	putchar((num2 % 10) + 0);
 if (num == 8 && num2 == 9)
 continue;
 	putchar(',');
