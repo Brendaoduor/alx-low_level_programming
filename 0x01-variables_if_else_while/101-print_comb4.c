645418a1bdfea9892fac68a1a0128a736790d985
brendaoduor <bresharchie2@gmail.com>
2022-07-02 14:34:02 -0700

    work3

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,22 +10,22 @@ int main(void)
 int num;
 int num1;
 int num3;
-for (num = 0; num < 8; num++)
+for (num = 0; num < 9; num++)
 {
-for (num1 = num + 1; num < 9; num++)
+for (num1 = 1; num < 9; num++)
 {
-for (num3 = num1 + 1; num1 < 10; num1++)
+for (num3 = 2; num1 < 9; num1++)
 {
+	if (num1 > num && num3 > num1)
 	putchar(num);
 	putchar(num1);
 	putchar(num3);
-	if (num == 7 && num1 == 8 && num3 == 9)
-		continue;
+	if (num != 7 || num1 != 8 || num3 != 9)
 	putchar(',');
 	putchar(' ');
 }
 }
 }
-putchar('\n')
+putchar('\n');
 return (0);
 }
