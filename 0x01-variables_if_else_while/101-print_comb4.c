749b197297cbf9f944f0f5922862df3e3488995c
brendaoduor <bresharchie2@gmail.com>
2022-07-01 18:00:24 -0700

    work

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,14 +15,17 @@ for (num = 0; num < 8; num++)
 for (num1 = num + 1; num < 9; num++)
 {
 for (num3 = num1 + 1; num1 < 10; num1++)
-
+{
 	putchar(num);
 	putchar(num1);
 	putchar(num3);
+	if (num == 7 && num1 == 8 && num3 == 9)
+		continue;
 	putchar(',');
 	putchar(' ');
 }
 }
-	putchar('\n')
+}
+putchar('\n')
 return (0);
 }
--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,8 +13,13 @@ for (num = 0; num < 98; num++)
 {
 for (num1 = num + 1; num < 99; num1++)
 {
-	putchar(num);
-	putchar(num1);
+	putchar((num / 10) + '0');
+	putchar((num % 10) + '0');
+	putchar(' ');
+	putchar((num1 / 10) + '0');
+	putchar((num1 % 10) + '0');
+	if(num == 98 && num1 ==99)
+		continue;
 	putchar(',');
 	putchar(' ');
 }
