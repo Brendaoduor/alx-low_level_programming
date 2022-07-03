82dc6bace997ac8a1ab16a23225abfddff519cba
brendaoduor <bresharchie2@gmail.com>
2022-07-03 14:51:55 -0700

    work done

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,23 +10,25 @@ int main(void)
 int num1;
 int num2;
 int num3;
-for (num1 = 0; num1 < 8; num1++)
+for (num1 = 48; num1 < 58; num1++)
 {
-for (num2 = num1 + 1; num2 < 9; num2++)
+for (num2 = 49; num2 < 58; num2++)
 {
-for (num3 = num2 + 1; num3 < 10; num3++)
+for (num3 = 50; num3 < 58; num3++)
 {
-	putchar((num1 % 10) + '0');
-	putchar((num2 % 10) + '0');	
-	putchar((num3 % 10) + '0');
-if (num1 == 7 && num2 == 8 && num3 == 9)
-continue;
+if (num3 > num2 && num2 > num1)
+{	
+	putchar(num1);
+	putchar(num2);	
+	putchar(num3);
+if (num1 != 55 || num2 != 56)
+{
 	putchar(',');
 	putchar(' ');
 }
 }
 }
-putchar('\n');
+}
+	putchar('\n');
 return (0);
 }
-
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,13 +10,9 @@ int main(void)
 int letter;
 int a;
 for (a = 0; a < 10; a++)
-{
 	putchar(a);
-}
 for (letter = 'a'; letter <= 'f'; letter++)
-{
 	putchar(letter);
-}
 	putchar('\n');
 return (0);
 }
