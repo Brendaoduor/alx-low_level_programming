e836a8ea95c2e75c8b3cb4e7d331c8de92b71b97
brendaoduor <bresharchie2@gmail.com>
2022-07-05 13:33:48 -0700

    10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,9 +6,10 @@
 void print_alphabets_x10(void)
 {
 char a;
-if (a <= 10)
-for (a = 'a'; a <= 'z'; a++)
+int m;
+for (m = 0; m <= 10; m++)
 {
+for (a = 'a'; a <= 'z'; a++)
 _putchar(a);
 _putchar('\n');
 }
