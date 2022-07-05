fefa4aa8f538b93348290b18c4f35303abb8f928
brendaoduor <bresharchie2@gmail.com>
2022-07-05 16:59:23 -0700

    work

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,10 +6,10 @@
 void times_table(void)
 {
 int a, b, prod;
-for (a = 0; a <= 9 a++)
+for (a = 0; a <= 9; a++)
 {
 _putchar('0');
-for (b = 1; b <= 9 b++)
+for (b = 1; b <= 9; b++)
 {
 _putchar(',');
 _putchar(' ');
