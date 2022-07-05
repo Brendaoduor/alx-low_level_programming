e44843b48286dd3955042b7e84b3a4906702d09b
brendaoduor <bresharchie2@gmail.com>
2022-07-05 13:04:13 -0700

    alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,8 +2,6 @@
 
 /**
  * print_alphabet - Prints lowercase alphabets
- *
- * Return: Always 0
  */
 void print_alphabet(void)
 {
@@ -11,6 +9,5 @@ char a;
 for (a = 'a'; a <= 'z'; a++)
 _putchar('a');
 _putchar('\n');
-return (0);
 }
 
