3b8ebc8a57bd8dce6cea9f5babf8d411c9c811fb
brendaoduor <bresharchie2@gmail.com>
2022-07-06 09:44:38 -0700

    work

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,14 +10,14 @@ int n;
 unsigned long a = 0, b = 1, sum;
 for (n = 0; n <= 50; n++)
 {
+sum = a + b;
 printf("%lu", sum);
 a = b;
 b = sum;
-sum = a + b;
+}
 if (n == 49)
 printf("\n");
 else
 printf(", ");
-}
 return (0);
 }
