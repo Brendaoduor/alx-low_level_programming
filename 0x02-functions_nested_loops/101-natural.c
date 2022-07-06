f8b6b58203af517e227e3e96861b209c53ff27c3
brendaoduor <bresharchie2@gmail.com>
2022-07-06 08:42:34 -0700

    work

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+int sum = 0;
+int a;
+for (a = 0; a < 1024; a++)
+if (a % 3 == 0 || a % 5 == 0)
+sum += a;
+{
+printf("%d\n", sum);
+}
+return (0);
+}
