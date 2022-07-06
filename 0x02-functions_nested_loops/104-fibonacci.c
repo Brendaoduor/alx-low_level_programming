6d166652a4b108d3d4ebbedfabfa1fb6b213e56c
brendaoduor <bresharchie2@gmail.com>
2022-07-06 14:06:32 -0700

    work

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+unsigned long int b = 1, c = 2, sum;
+for (a = 0; a <= 98 a++)
+if (a != 98)
+{
+printf("%lu, ", sum)
+}
+else
+{
+printf("%lu\n", sum)
+}
+sum = b + c;
+b = c;
+c = sum;
+return (0);
+}
