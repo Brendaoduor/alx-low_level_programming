1bebc742936f445ff31fadc3c4ba06220152a5cf
brendaoduor <bresharchie2@gmail.com>
2022-07-05 17:28:15 -0700

    work

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the value fo last digit
+ *
+ * @z: passes parameter
+ *
+ * Return:  returns the value of z
+ */
+int print_last_digit(int z)
+{
+if (z < 0)
+
+z = -z;
+
+return (z);
+}
+
