266dab08f34214f6780878abe0f6e4fc72c2c68b
brendaoduor <bresharchie2@gmail.com>
2022-07-07 04:55:34 -0700

    nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - function for conducting multiplication between two numbers
+ * @a: pass parameters
+ * @b: pass parameters
+ * Return: returns 0
+ */
+int mul(int a, int b)
+{
+int n1, n2, prod;
+prod = n1 * n2;
+printf("%d", prod);
+return (0);
+}
+
