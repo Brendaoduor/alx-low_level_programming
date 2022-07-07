0eb71c2bc9adebbf7986280f348bece0e49943fb
brendaoduor <bresharchie2@gmail.com>
2022-07-07 14:57:47 -0700

    nested

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,18 +1,25 @@
 #include "main.h"
 
 /**
- *
- *
+ * print_square - prints a square in the terminal using the #
+ * @size: size of the square
  */
 void print_square(int size)
 {
+int sq1;
 int sq;
 if (size > 0)
-for (sq = 0; sq <= 4; sq++)
+for (sq = 0; sq <= size; sq++)
+{
+for (sq1 = 0; sq1 <= size; sq1++)
 {
 _putchar('#');
+{
 _putchar('\n');
 }
+}
+else
 _putchar('\n');
+}
 }
 
