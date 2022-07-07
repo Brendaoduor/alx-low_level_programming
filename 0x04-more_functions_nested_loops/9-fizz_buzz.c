7074709fcf170d6f4670caa9e88086dac70668ad
brendaoduor <bresharchie2@gmail.com>
2022-07-07 16:37:09 -0700

    nested

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,8 +15,6 @@ if (num % 3 == 0 && num % 5 != 0)
 	printf("Fizz ");
 else if (num % 5 == 0 && num % 3 != 0)
 	printf("Buzz ");
-else if (num == 100)
-	printf("Buzz");
 else if (num % 3 == 0 && num % 5 == 0)
 	printf("FizzBuzz ");
 else
