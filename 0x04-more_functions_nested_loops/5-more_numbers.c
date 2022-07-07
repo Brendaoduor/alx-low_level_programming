f74169b38bfffc60c914364664261ac493561f75
brendaoduor <bresharchie2@gmail.com>
2022-07-07 14:41:24 -0700

    nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,6 +14,6 @@ if (b >= 10)
 _putchar((b / 10) + '0');
 _putchar((b % 10) + '0');
 }
-}
 _putchar('\n');
+}
 }
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+void print_square(int size)
+{
+int sq;
+if (size > 0)
+for (sq = 0; sq <= 4; sq++)
+{
+_putchar('#');
+_putchar('\n');
+}
+_putchar('\n');
+}
+
