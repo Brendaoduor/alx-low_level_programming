acb72a5a915168380fed9606d93b20e35b1f8dd1
brendaoduor <bresharchie2@gmail.com>
2022-07-08 15:06:07 -0700

    nest

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,10 +14,13 @@ if (num % 3 == 0 && num % 5 != 0)
 	printf("Fizz ");
 else if (num % 5 == 0 && num % 3 != 0)
 	printf("Buzz ");
+else if (num == 100)
+	printf("Buzz");
 else if (num % 3 == 0 && num % 5 == 0)
 	printf("FizzBuzz ");
+else
+	printf("%d ", num);
 }
 printf("\n");
 return (0);
 }
-
