9fbd90cb452b1574a1b5eb820c2b11211e529a99
brendaoduor <bresharchie2@gmail.com>
2022-07-11 07:39:36 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+include "main.h"
+
+/**
+ *swap_int - swaps the value of two integers
+ * @a:pointer memory
+ * @b:memory locations that point to the actual variable
+ */
+void swap_int(int *a, int *b)
+{
+int x;
+int y;
+	swap(&x, &y);
+	_putchar(x);
+	_putchar(y);
+}
