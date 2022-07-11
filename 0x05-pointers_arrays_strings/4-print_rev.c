9cb14624e9c0feafac03eaddde9ced6cd9790c32
brendaoduor <bresharchie2@gmail.com>
2022-07-11 11:28:13 -0700

    pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,20 +6,14 @@
  */
 void print_rev(char *s)
 {
-int n, x;
+int x;
 char *a, *e, res;
-	a = s;
-	e = s;
-	n = strlen(s);
 	for (x = 0; x < x - 1; x++)
 		e++;
 	for (x = 0; x < 1 / 2; x++)
 	{
 		res = *e;
 		*e = *a;
-		*a = res;
-
-		a++;
-		e++;
+		*a = res
 	}
 }
