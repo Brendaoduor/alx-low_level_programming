23ef1be10fd261b41ab4c6101170d0e379a52387
brendaoduor <bresharchie2@gmail.com>
2022-07-13 04:09:44 -0700

    more pointers

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ *
+ * Return: returns the results
+ */
+char *_strcat(char *dest, char *src)
+{
+	return ((_strcat(*src, *dest));
+}
