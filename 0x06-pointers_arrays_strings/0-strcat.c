7bfc5d08f0a99a6481155b3e39210f089dd12f33
brendaoduor <bresharchie2@gmail.com>
2022-07-13 13:12:35 -0700

    pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,13 +2,16 @@
 #include <string.h>
 
 /**
- * strcat - this function enables concatenation between two strings
+ * _strcat - this function enables concatenation between two strings
+ * @dest: points to the destination of concatinated string
+ * @src: points to the string which is to be concatinated
  * Return: returns the result after concatenation
  */
 char *_strcat(char *dest, char *src)
 {
-	char var = 0;
-	char var1 = 0;
+char var = 0;
+char var1 = 0;
+
 	while (dest[var])
 		var++;
 	for (var = 0; src[var]; var++;)
