a35be8e588706ab1d1fbbb4a8990d1b0e2be73eb
brendaoduor <bresharchie2@gmail.com>
2022-07-15 02:16:14 -0700

    fixed code

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,30 +7,26 @@
  */
 int main(void)
 {
-int a, b, c, d, e, f;
-for (e = 0; e < 100; e++)
+int num1, num2;
+for (num1 = 0; num1 <= 98; num1++)
 {
-a = e / 10;
-b = e % 10;
-for (f = 0; f < 100; f++)
+for (num2 = num1 + 1; num2 <= 99; num2++)
 {
-c = f / 10;
-d = f % 10;
-if (a < c || (a == c && b < d))
-{
-putchar(a + '0');
-putchar(b + '0');
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
+
 putchar(' ');
-putchar(c + '0');
-putchar(d + '0');
-{
+
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
+
+if (num1 == 98 && num2 == 99)
+continue;
+
 putchar(',');
 putchar(' ');
-}
-}
 }
 }
 putchar('\n');
 return (0);
 }
-
