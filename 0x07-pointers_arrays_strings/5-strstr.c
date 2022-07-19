3f9ffd1d0bbbad58ba641557d5aab9ab8889b59c
brendaoduor <bresharchie2@gmail.com>
2022-07-19 16:20:17 -0700

    pointers

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,6 +9,8 @@
 char *_strstr(char *haystack, char *needle)
 {
 int n;
+if (*needle == 0)
+	return (haystack);
 while (*haystack)
 {
 n = 0;
@@ -18,10 +20,9 @@ n = 0;
 		if (needle[n + 1] == '\0')
 			return (haystack);
 		n++;
-	}
-	while (haystack[n] == needle[n])
+	} while (haystack[n] == needle[n]);
 	}
 		haystack++;
-	}
+}
 return ('\0');
 }
