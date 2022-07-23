4b439b7ee4d5bffd489cbe4fff4d14d41d4fac12
brendaoduor <bresharchie2@gmail.com>
2022-07-22 17:54:13 -0700

    argc/argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,9 +9,7 @@
 int main(int argc, char *argv[])
 {
 int a;
-	for (a = 0; a < argc; a++)
-	{
-		printf("argv[%d] = %s\n", a, argv[a]);
-	}
-	return (0);
+for (a = 0; a < argc; a++)
+printf("%s\n", argv[a]);
+return (0);
 }
