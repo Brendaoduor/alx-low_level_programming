e8f86345b630d012d2d3f90b682ec8191d09a737
brendaoduor <bresharchie2@gmail.com>
2022-07-28 13:03:09 -0700

    malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <strings.h>
+#include <stdlib.h>
 
 /**
  * malloc_checked - allocates memory using malloc
@@ -8,10 +8,8 @@
  */
 void *malloc_checked(unsigned int b)
 {
-int *n;
-int size = 0;
-n = malloc(sizeof(int) * size);
-n[0] = 98;
-_putchar(n);
-return (0);
+int *n = malloc(b);
+if (n == NULL)
+	exit(98);
+return (n);
 }
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: string to be concatenated
+ * @s2: the second string to be concatented
+ * @n:size of the strings
+ * Return: always 0;
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+int size = n;
+char *str;
+int string;
+if (s1 == NULL)
+	s1 = "";
+if (s2 == NULL)
+	s2 = "";
+for (string = 0; s1[string]; string++)
+	size++;
+str = malloc(sizeof(char) * (size + 1));
+if (str == NULL)
+	return (NULL);
+size = 0;
+for (string = 0; s1[string]; string++)
+	str[size++] = s1[string];
+for (string = 0; s2[string] && string < n; string++)
+	str[size++] = s2[string];
+str[size] = '\0';
+return (str);
+}
