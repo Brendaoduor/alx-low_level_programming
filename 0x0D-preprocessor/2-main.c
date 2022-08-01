8678afc4956d72d339dc47519c633ea445c6fe49
brendaoduor <bresharchie2@gmail.com>
2022-08-01 13:30:44 -0700

    macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function that returns the name of the current file
+ * Return: always 0 success
+ */
+int main(void)
+{
+	_putchar(__FILE__);
+	_putchar("%s\n");
+return (0);
+}
