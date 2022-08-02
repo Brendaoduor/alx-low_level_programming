17b0c2abb91bee6989a93ea77c1e888859999907
brendaoduor <bresharchie2@gmail.com>
2022-08-02 13:57:07 -0700

    structures

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,7 +18,7 @@ if (d->name == NULL)
 	printf("name: (nil)\n");
 else
 	printf("name: %s\n", d->name);
-	printf("age: %f\n", d->age);
+printf("age: %f\n", d->age);
 if (d->owner == NULL)
 	printf("owner: (nil)\n");
 else
