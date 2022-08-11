ca806f110db953679dd2343515a64c4827d45ad1
brendaoduor <bresharchie2@gmail.com>
2022-08-11 15:17:01 -0700

    linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,13 +11,25 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t lastnode;
+	int len = 0;
 	list_t *newnode = malloc(sizeof(list_t));
 
+	if (str == NULL)
+		return (NULL);
+
 	newnode->str = strdup(str);
+	if (newnode->str == NULL)
+		return (NULL);
+	while (str[len])
+		len++;
+	newnode->len = len;
 	newnode->next = NULL;
+
 	if (*head == NULL)
 	{
 	*head == newnode;
+	return (*head);
 	}
 	else
 	{
