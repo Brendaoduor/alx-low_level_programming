ccd09af5842e435e03335d363701b24b6f469101
brendaoduor <bresharchie2@gmail.com>
2022-08-11 17:45:27 -0700

    assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,18 @@
 section .text
-	global _start
+extern printf
+	global main, _start
+main:
 _start:
+	mov edi, fmt
 	mov edx, len
 	mov ecx, msg
 	mov ebx, 1
-	mov eax, 4 ;system call
+	mov eax, 0 ;system call
+	call printf
 	int 0x80
 	mov eax, 1 ;system exit
-	int 0x80
+	int 0x80 ;linux kernel service
 section .data
 	msg db "Hello, Holberton", 10
 	len equ $ -msg
+	fmt db "%s", 10, 0
