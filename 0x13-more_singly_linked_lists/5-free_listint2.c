1697c1c8f1143ccfea2314b8d2916a4819f13e16
brendaoduor <bresharchie2@gmail.com>
2022-08-15 13:14:05 -0700

    complete task

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,16 +7,15 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp, current;
+	listint_t *tmp;
 
-	current = *head;
 	if (*head == NULL)
 		return;
 	while (*head)
 	{
-		tmp = current->next;
-		free(current);
-		current = tmp;
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
 	}
-	current = NULL;
+	head = NULL;
 }
