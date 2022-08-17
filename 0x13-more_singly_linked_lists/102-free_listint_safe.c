0094059826930ee314e86ec4892f83a2f4e54953
brendaoduor <bresharchie2@gmail.com>
2022-08-16 17:21:50 -0700

    code

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,60 +2,34 @@
 #include <stdlib.h>
 
 /**
- * _ra -relocates memory of an array pointer
- * @size: size of the list to be appended to
- * @new: the new node to add to the list
- * @list: the old list
- * Return: always 0
- */
-listint_t **_ra(listint_t **list, size_t size, const listint_t *new)
-{
-	listint_t **newlist;
-	size_t i;
-
-	newlist = malloc(size * sizeof(listint_t *));
-	if (newlist == NULL)
-	{
-		free(list);
-		exit(98);
-	}
-	for (i = 0; i < size - 1; i++)
-		newlist[i] = list[i];
-	list[i] == new;
-	free(list);
-	return (newlist);
-}
-
-/**
  * free_listint_safe- frees a list
  * @h: the first node of the list
  * Return: always 0
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t i, num = 0;
-	listint_t **list = NULL;
-	listint_t *next;
+	size_t len = 0;
+	int diff;
+	listint_t *temp;
 
-	if (h == NULL || *h == NULL)
-		return (num);
-	while (*h != NULL)
+	if (!h || !*h)
+		return (0);
+	while (*h)
 	{
-		for (i = 0; i < num; i++)
+		diff = *h - (*h)->next;
+		if (diff > 0)
 		{
-			if (*h == list[i])
-			{
-				*h = NULL;
-				free(list);
-				return (num);
-			}
+			temp = (*h)->next;
+			*h = temp;
+			len++;
 		}
-		num++;
-		list = _ra(list, num, *h);
-		next = (*h)->next;
-		free(*h);
-		*h = next;
+		else
+		{
+			*h = NULL;
+			len++;
+			break;
+		}
 	}
-	free(list);
-	return (num);
+	*h = NULL;
+	return (len);
 }
