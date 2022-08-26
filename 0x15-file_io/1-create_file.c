6ba36c79a958083df260ea493aa1b97fdf514c80
brendaoduor <bresharchie2@gmail.com>
2022-08-26 05:37:50 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,6 +25,7 @@ int create_file(const char *filename, char *text_content)
 	r = read(fd, buffer, *text_content);
 	w = write(STDOUT_FILENO, buffer, r);
 
+	buffer[r] = '\0';
 	free(buffer);
 	close(fd);
 	return (w);
