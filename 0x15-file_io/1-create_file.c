0d26dfe42f243a8191f921746b857678d33092a2
brendaoduor <bresharchie2@gmail.com>
2022-08-26 05:34:42 -0700

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: is the name of the file to be created
+ * @text_content: NULL terminated string to write to the file
+ * Return: returns 1 if success or -1 if failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, r, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
+
+	if (fd == -1)
+		return (-1);
+	if (buffer == NULL)
+	{
+		open(text_content, O_CREAT);
+	}
+
+	r = read(fd, buffer, *text_content);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	free(buffer);
+	close(fd);
+	return (w);
+}
