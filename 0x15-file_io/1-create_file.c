493a46d4cbe311fece35021ab93db98d792547a9
brenda oduor <bresharchie2@gmail.com>
2023-05-09 02:48:37 +0300

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,13 +12,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
-		fdw = write(fd, text_content);
+		fdw = write(fd, text_content, content_len);
 		len = strlen(text_content);
 	}
 	if (fdw != len)
