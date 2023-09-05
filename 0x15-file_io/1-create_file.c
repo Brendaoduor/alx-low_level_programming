00815e3111e0d524797b6304386f55777086d9c5
brendaoduor <bresharchie2@gmail.com>
2023-09-04 18:04:53 -0700

    updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,15 +16,20 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
-	while (text_content != NULL && text_content[len] != '\0')
-		len++;
-
-		fdw = write(fd, text_content, content_len);
-
-	if (fd == -1 | fdw == -1)
+	while (text_content != NULL)
 	{
+		len = 0;
+		while(text_content[len] != '\0')
+			len++;
+
+		fdw = write(fd, text_content, len);
+
+		if (fdw != len)
+		{
 		close(fd);
 		return (-1);
+		}
 	}
+	close(fd);
 	return (1);
 }
